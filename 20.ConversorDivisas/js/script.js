d279d776946e621abf8c30192a53fee3603769ae
LexieDev <agomartinez93#gmail.com>
2025-07-02 11:37:08 +0200

    version 1.0

---
+++ 20.ConversorDivisas/js/script.js
@@ -0,0 +1,61 @@
+
+
+function convertirMoneda() {
+    
+    var origen = document.querySelector("#origen").value;
+    var cantidad = document.querySelector("#cantidad").value;
+    var destino = document.querySelector("#destino").value;
+    var resultado = 0;
+    
+    if (origen == "euros" && destino == "dolares") {
+       resultado = cantidad * 1.177;
+    } else if (origen == "euros" && destino == "libras") {
+        resultado = cantidad * 0.859;
+    } else if (origen == "euros" && destino == "pesetas"){
+        resultado = cantidad *166.386;
+    } else if (origen == "euros" && destino == "euros") {
+        resultado = cantidad
+    }
+    document.querySelector("#resultado").value = resultado;
+
+
+    if (origen == "dolares" && destino == "libras") {
+       resultado = cantidad * 0.729;
+    } else if (origen == "dolares" && destino == "euros") {
+        resultado = cantidad * 0.849;
+    } else if (origen == "dolares" && destino == "pesetas"){
+        resultado = cantidad *141.259;
+    }else if (origen == "dolares" && destino == "dolares") {
+        resultado = cantidad
+    }
+    document.querySelector("#resultado").value = resultado;
+
+
+    if (origen == "libras" && destino == "dolares") {
+       resultado = cantidad * 1.163;
+    } else if (origen == "libras" && destino == "euros") {
+        resultado = cantidad * 1.163;
+    } else if (origen == "libras" && destino == "pesetas"){
+        resultado = cantidad * 193.539;
+    } else if (origen == "libras" && destino == "libras") {
+        resultado = cantidad
+    }
+    document.querySelector("#resultado").value = resultado;
+
+
+    if (origen == "pesetas" && destino == "dolares") {
+       resultado = cantidad * 0.00708;
+    } else if (origen == "pesetas" && destino == "euros") {
+        resultado = cantidad * 0.00601;
+    } else if (origen == "pesetas" && destino == "libras"){
+        resultado = cantidad * 0.00517;
+    } else if (origen == "pesetas" && destino == "pesetas") {
+        resultado = cantidad
+    } 
+    document.querySelector("#resultado").value = resultado;
+}
+// conssole.log(origen);
+// conole.log(cantidad);
+// console.log(destino);
+// console.log(resultado);
+// 
